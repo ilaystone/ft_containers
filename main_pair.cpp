304d7ca0454ea7d306541abf9f4d4d2db7eb5442
ikhadem <khadem.ilyass@gmail.com>
2022-01-06 05:12:08 +0100

    pair class and make_pair done

--- main_pair.cpp
+++ main_pair.cpp
@@ -6,6 +6,7 @@
 	namespace ft = std;
 #endif
 #include <iostream>
+#include <string>
 
 int		main(void)
 {
@@ -32,10 +33,10 @@ int		main(void)
 	if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
 	if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
 
-	foo = ft::make_pair(10,20);
-	bar = ft::make_pair(10.5,'A'); // ok: implicit conversion from pair<double,char>
+	ft::pair<int,int> foo1 = ft::make_pair(10,20);
+	ft::pair<int,int> bar1 = ft::make_pair(10.5,'A'); // ok: implicit conversion from pair<double,char>
 
-	std::cout << "foo: " << foo.first << ", " << foo.second << '\n';
-	std::cout << "bar: " << bar.first << ", " << bar.second << '\n';
+	std::cout << "foo: " << foo1.first << ", " << foo.second << '\n';
+	std::cout << "bar: " << bar1.first << ", " << bar.second << '\n';
 	return (0);
 }
